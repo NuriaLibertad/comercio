9ab0da04da4896f40231784c086972b2c1a33d47
NuriaLibertad <nurialibertad29@gmail.com>
2023-06-05 09:14:05 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -0,0 +1,15 @@
+package com.itb.inf2hm.comercio.model;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes
+	// private : Propriedade (atributo ou método) só pode ser acessando
+	//          dentro da própria classe
+	
+
+}
