3df3a858e2e077a17e27286d3d53073db379b071
Nuria Libertad <nurialibertad29@gmail.com>
2023-06-21 11:09:43 -0300

    Edicao

--- src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
@@ -1,58 +1,89 @@
-package com.itb.info2hm.comercio.controller;
+package com.itb.inf2hm.comercio.controller;
 
  
 
 import java.util.ArrayList;
+
 import java.util.List;
 
  
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+ 
 
 import com.itb.inf2hm.comercio.model.Produto;
 
  
+
 @Controller
+
 @RequestMapping("/comercio/produtos")
+
 public class LojaController {
 
- 
 
-  List<Produto> produtos = new ArrayList<Produto>();    
+    List<Produto> listaDeProdutos = new ArrayList<Produto>();
+
 
     @GetMapping("/listar")
+
     public String listarProdutos(Model model) {
 
-        Produto produto = new Produto();
-        produto.setId(1451);
-        produto.setNome("televisor 70' samsung");
-        produto.setCodigoBarras("GA5LAXIA456");
-        produto.setPreco(6541.25);
-        
-        Produto produto2 = new Produto();
-        produto.setId(1451);
-        produto.setNome("máquina de Lavar Brastemp");
-        produto.setCodigoBarras("LJFEFDOENDS5647");
-        produto.setPreco(4321.54);
-        
-        //Adicionando os produtos no ArrayList
-        
-        produtos.add(produto);
-        produtos.add(produto2);
-        
 
+        Produto produto = new Produto();
+
+        produto.setId(145l);
+
+        produto.setNome("Televisor 70' Samsung");
+
+        produto.setCodigoBarras("JFALJFDOE2514");
+
+        produto.setPreco(6541.25);
+
+
+        Produto produto2 = new Produto();
+
+        produto2.setId(146l);
+
+        produto2.setNome("Máquina de Lavar Brastemp");
+
+        produto2.setCodigoBarras("JFELJFDOA2910");
+
+        produto2.setPreco(4321.54);
+
+
+      // Adicionando os produtos no Arraylist
+
+        listaDeProdutos.add(produto);
+
+        listaDeProdutos.add(produto2);
+
+
+        model.addAttribute("listaDeProdutos", listaDeProdutos);
 
         return "produtos";
 
-    }    
+    }
+
+
     @GetMapping("/novo-produto")
+
+
     public String novoProduto () {
 
         return "novo-produto";
+
+ 
+
     }
+
+ 
+
 }
-
-
--- src/main/java/com/itb/inf2hm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -2,10 +2,34 @@ package com.itb.inf2hm.comercio.model;
 
 public class Produto {
 	
-	private long id;
+	private Long id;
 	private String nome;
 	private String codigoBarras;
 	private double preco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 	
 	// public : Acesso liberado para todas as classes
 	// private : Propriedade (atributo ou método) só pode ser acessando
